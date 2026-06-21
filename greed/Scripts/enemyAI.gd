ce95db6445887e2128f46d8e536bdc145bb3805f
8bitbiochemist@gmail.com <8bitbiochemist@gmail.com>
2026-06-21 16:07:40 +0100

    Update enemyAI.gd

--- greed/Scripts/enemyAI.gd
+++ greed/Scripts/enemyAI.gd
@@ -1,4 +1,21 @@
 extends CharacterBody2D
-	
+@export var wander_speed: float = 60.0
+@export var chase_speed: float = 75.0
+@export var attack_damage: int=12
+@export var detection_radius: float = 80.0
+@export var wander_radius: float = 75
+
+@export var agent: NavigationAgent2D
+
+
+var spawn_position: Vector2
+enum {Patrol, detected}
+
+var state_machine
+var player: Node = null
+var direction: Vector2 = Vector2.ZERO
+
+func _ready():
+	spawn_position = global_position
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
